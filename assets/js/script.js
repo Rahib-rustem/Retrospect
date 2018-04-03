2c2af7c79b97fe86b670444ebcdaa15b5d279766
Rahib <rahib.rustemzade@gmail.com>
2018-04-03 21:29:25 +0400

    addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     $('.homeMenu').click(function(){
-       $('.homeContent').css({'right':'0','position':'fixed','top':'0px'})
+       $('.homeContent').css({'right':'0','position':'fixed','top':'0px','z-index':'99999'})
     });
     $('.button').click(function(){
         $('.homeContent').css({'right':'-295px'})
